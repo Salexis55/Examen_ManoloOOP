a5b6cf4fd3c70c6bfd57d7d16c97d3a1aebdad0a
adayxgt <adayxgt@gmail.com>
2025-10-27 10:41:47 +0100

    commit cliente

---
+++ src/examen_manolo/Cliente.java
@@ -0,0 +1,18 @@
+package examen_manolo;
+
+
+public class Cliente {
+	    private String nombre;
+	    private String documento;
+
+	    public Cliente(String nombre, String documento) {
+	        this.nombre = nombre;
+	        this.documento = documento;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return nombre + " (DNI: " + documento + ")";
+	    }
+	}
+
