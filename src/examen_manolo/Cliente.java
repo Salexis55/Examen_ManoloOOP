ddb63b2b54deb2448702fcd84cf7829d3d376268
Salexis55 <a.garciaperez@colegiovirgendeeuropa.edu.es>
2025-10-31 09:50:58 +0100

    comentarioscliente

--- src/examen_manolo/Cliente.java
+++ src/examen_manolo/Cliente.java
@@ -1,18 +1,21 @@
 package examen_manolo;
 
-
+// Clase que representa a un cliente
 public class Cliente {
-	    private String nombre;
-	    private String documento;
 
-	    public Cliente(String nombre, String documento) {
-	        this.nombre = nombre;
-	        this.documento = documento;
-	    }
+    // Atributos del cliente
+    private String nombre;     // Nombre del cliente
+    private String documento;  // Documento de identidad (DNI)
 
-	    @Override
-	    public String toString() {
-	        return nombre + " (DNI: " + documento + ")";
-	    }
-	}
+    // Constructor: inicializa los datos del cliente
+    public Cliente(String nombre, String documento) {
+        this.nombre = nombre;
+        this.documento = documento;
+    }
 
+    // Método toString: devuelve una representación en texto del cliente
+    @Override
+    public String toString() {
+        return nombre + " (DNI: " + documento + ")";
+    }
+}
