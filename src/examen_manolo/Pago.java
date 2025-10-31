4810b36987c703713009fd40b69bda0e13f1968c
Salexis55 <a.garciaperez@colegiovirgendeeuropa.edu.es>
2025-10-31 10:01:26 +0100

    comentariospago

--- src/examen_manolo/Pago.java
+++ src/examen_manolo/Pago.java
@@ -1,23 +1,27 @@
-
 package examen_manolo;
 
 /**
- * @author marcos 27 oct 2025
+ * Clase que representa un pago realizado por un alquiler o venta.
+ * @author marcos
+ * @date 27 oct 2025
  */
 public class Pago {
+
+    // Monto total del pago
     private double monto;
+
+    // Método utilizado (por ejemplo: tarjeta, efectivo, etc.)
     private String metodo;
 
+    // Constructor: inicializa los datos del pago
     public Pago(double monto, String metodo) {
         this.monto = monto;
         this.metodo = metodo;
     }
 
+    // Método toString: devuelve una descripción legible del pago
     @Override
     public String toString() {
         return "Pago de $" + monto + " con método: " + metodo;
     }
 }
-
-
-
