261831ddabd2946707bc632a92fe96b33ac41f24
Salexis55 <a.garciaperez@colegiovirgendeeuropa.edu.es>
2025-10-31 09:51:47 +0100

    comentariosempleado

--- src/examen_manolo/Empleado.java
+++ src/examen_manolo/Empleado.java
@@ -1,14 +1,19 @@
 package examen_manolo;
 
+// Clase que representa a un empleado de la empresa
 public class Empleado {
-    private String nombre;
-    private String idEmpleado;
 
+    // Atributos del empleado
+    private String nombre;      // Nombre del empleado
+    private String idEmpleado;  // Identificador único del empleado
+
+    // Constructor: inicializa los datos del empleado
     public Empleado(String nombre, String idEmpleado) {
         this.nombre = nombre;
         this.idEmpleado = idEmpleado;
     }
 
+    // Método toString: devuelve una representación en texto del empleado
     @Override
     public String toString() {
         return nombre + " (ID: " + idEmpleado + ")";
