6918e9d02971a0cfffeae161db4c6ca7fa72fb74
Salexis55 <a.garciaperez@colegiovirgendeeuropa.edu.es>
2025-10-31 09:59:57 +0100

    comentariossedan

--- src/examen_manolo/SEDAN.java
+++ src/examen_manolo/SEDAN.java
@@ -1,19 +1,24 @@
 package examen_manolo;
 
+// La clase SEDAN hereda de Vehiculos
+public class SEDAN extends Vehiculos {
 
-	public class SEDAN extends Vehiculos {
-	    private boolean lujo;
+    // Indica si el sedán es de lujo o no
+    private boolean lujo;
 
-	    public SEDAN(String matricula, String marca, String modelo, double precioPorDia, boolean lujo) {
-	        super(matricula, marca, modelo, precioPorDia);
-	        this.lujo = lujo;
-	    }
+    // Constructor: inicializa los datos del sedán
+    public SEDAN(String matricula, String marca, String modelo, double precioPorDia, boolean lujo) {
+        // Llama al constructor de la clase padre (Vehiculos)
+        super(matricula, marca, modelo, precioPorDia);
+        this.lujo = lujo;
+    }
 
-	    @Override
-	    public double calcularPrecio(int dias) {
-	        double extra = lujo ? 1.10 : 1.0; // 10% más si es de lujo
-	        return super.calcularPrecio(dias) * extra;
-	    }
-	}
-	
-
+    // Sobrescribe el método calcularPrecio de la clase padre
+    @Override
+    public double calcularPrecio(int dias) {
+        // Si es de lujo, se aplica un 10% extra al precio total
+        double extra = lujo ? 1.10 : 1.0;
+        // Devuelve el precio base multiplicado por el factor extra
+        return super.calcularPrecio(dias) * extra;
+    }
+}
